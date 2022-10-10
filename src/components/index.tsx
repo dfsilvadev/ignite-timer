eb1f8270b8d6e38af953b1e37a9c2fbd7c75bb45
Daniel Silva <dfsilva.dxp@gmail.com>
2022-10-10 11:01:23 -0300

    feat: add export default

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,3 +2,4 @@ export { default as Button } from "./Button";
 export { default as Countdown } from "./Countdown";
 export { default as Head } from "./Head";
 export { default as Header } from "./Header";
+export { default as HistoryList } from "./HistoryList";
